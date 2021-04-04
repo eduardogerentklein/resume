267ff808dd552a70a1449ea64763805a6dc9474d
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2021-04-04 11:06:26 -0300

    Adding next-seo

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import '../styles/tailwind.css'
 
 import Layout from '../components/Layout.js'
+import configSeo from '../seo.json';
+
+import { DefaultSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <DefaultSeo {...configSeo} />
       <Component {...pageProps} />
     </Layout>
   )
--- pages/resume.js
+++ pages/resume.js
@@ -75,7 +75,7 @@ export default function Resume() {
   return (
     <>
       <Head>
-        <title>Eduardo Klein - Resume</title>
+        <title>Eduardo Gerent Klein - Resume</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <main>
