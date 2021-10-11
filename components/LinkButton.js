253581856b044760e01984dff5e16c0e4e17402f
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2021-10-11 16:34:39 -0300

    Removing Link component from LinkButton's component

--- components/LinkButton.js
+++ components/LinkButton.js
@@ -5,11 +5,9 @@ import Description from '../components/Description.js'
 
 const LinkButton = ({ link }) => (
   <div className="flex items-center space-x-1">
-    <Link href={link.href}>
-      <a className="hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md p-2 dark:bg-gray-00">
-        <Paragraph>{link.name}</Paragraph>
-      </a>
-    </Link>
+    <a target="_blank" href={link.href} rel="noopener noreferrer" className="hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md p-2 dark:bg-gray-00">
+      <Paragraph>{link.name}</Paragraph>
+    </a>
     {link.description && <Description>{link.description}</Description>}
   </div>
 )
