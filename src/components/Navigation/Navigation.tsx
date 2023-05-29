14f1a8978c88e6d2a9e704237bbfd88bc369e5b9
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2023-05-29 16:40:18 +1200

    :wrench: fix: layout shift icons

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import Image from 'next/image'
 
@@ -7,11 +8,18 @@ import { NavigationLinkOptions, NavLink } from '@/types'
 import { Button, NavigationLink } from '@/components'
 
 export const Navigation = ({ links }: NavigationLinkOptions) => {
+  const [mounted, setMounted] = useState<boolean>(false)
   const { theme, setTheme } = useTheme()
 
   const handleClick = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
   }
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
 
   return (
     <nav className="mx-4 mb-16 mt-4 flex max-w-screen-lg items-center justify-between rounded-md bg-gray-200 px-6 py-3 dark:bg-gray-800 dark:bg-opacity-50 md:mx-auto">
